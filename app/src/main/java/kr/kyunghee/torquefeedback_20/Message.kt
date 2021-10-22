154e5175e61b693941b5892514c3c3f9d9e4a0e5
Leroochi <valeria.vic.park@gmail.com>
2021-10-22 16:11:55 +0900

    INIt

---
+++ app/src/main/java/kr/kyunghee/torquefeedback_20/Message.kt
@@ -0,0 +1,17 @@
+package kr.kyunghee.torquefeedback_20
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+class Message: Serializable {
+    @SerializedName("message")
+    @Expose
+    var message: String? = null
+}
+
+class Force: Serializable {
+    @SerializedName("message")
+    @Expose
+    var message: Float? = null
+}
